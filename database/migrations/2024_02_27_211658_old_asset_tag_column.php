eab6d90d4ecb7439591276a3adddeab99a22dee1
akemidx <kojotek.dx@gmail.com>
2024-02-27 16:32:31 -0500

    migration for new column

---
+++ database/migrations/2024_02_27_211658_old_asset_tag_column.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class OldAssetTagColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('assets', function (Blueprint $table) {
+            $table->string('old_asset_tag')->nullable()->default(null);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('assets', function (Blueprint $table) {
+            if(Schema::hasColumn('assets','old_asset_tag')) {
+                $table->dropColumn('old_asset_tag');
+            }
+        });
+    }
+}
