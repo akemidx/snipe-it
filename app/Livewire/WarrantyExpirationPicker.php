7dc3e124f311187a14d8ff698c898a2fd14057cb
akemidx <kojotek.dx@gmail.com>
2025-04-29 18:47:25 -0400

    getting months and purchase date.

--- app/Livewire/WarrantyExpirationPicker.php
+++ app/Livewire/WarrantyExpirationPicker.php
@@ -3,11 +3,13 @@
 namespace App\Livewire;
 
 use App\Models\AssetModel;
+use App\Models\Asset;
 use Livewire\Component;
 
 class WarrantyExpirationPicker extends Component
 {
     public $warranty_months = 0;
+    public $purchase_date = null;
     public function render()
     {
         return view('livewire.warranty-expiration-picker');
@@ -17,4 +19,12 @@ class WarrantyExpirationPicker extends Component
     {
         $this->warranty_months = AssetModel::find($id)->warranty_months;
     }
+    public function setPurchaseDateFromAsset($id)
+    {
+        $this->purchase_date = Asset::find($id)->purchase_date;
+
+        //if new then choose now
+        //if edit then grab it from asset id
+    }
+
 }
--- resources/views/livewire/warranty-expiration-picker.blade.php
+++ resources/views/livewire/warranty-expiration-picker.blade.php
@@ -16,16 +16,35 @@
 
         id 8 = 36 months
         id 1 = 24 months --}}
+    <input placeholder="date picker" name="setDate">
+
     <p>warranty_months: {{ $warranty_months }}</p>
+    <p>purchase_date: {{ $purchase_date }}</p>
 </div>
 
 @script
     <script>
         $('#model_select_id')
             .on('select2:select', function (event) {
+
                 //$wire.$set('warranty_months', event.params.data.id)
                 $wire.$call('setWarrantyMonthsFromModel', event.params.data.id)
                 console.log(event.params.data.id)
+
+                const input = document.querySelector("input");
+                const log = document.getElementById("log");
+
+                {{--input.addEventListener("change", setWarrantyDate);--}}
+
+                {{--function setWarrantyDate(e) {--}}
+                {{--    @if ($asset->purchase_date)--}}
+                {{--        {{ Helper::getFormattedDateObject($asset->present()->warranty_expires(), 'date', false) }}--}}
+                {{--        {{ Carbon::parse($asset->present()->warranty_expires())->diffForHumans(['parts' => 2]) }}--}}
+                {{--    @else--}}
+                {{--        {{ trans('general.na_no_purchase_date') }}--}}
+                {{--    @endif--}}
+                {{--}--}}
+                    $wire.$call('setPurchaseDateFromAsset', event.params.data.id)
             });
     </script>
 @endscript
